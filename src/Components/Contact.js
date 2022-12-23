4dafc9236960d6e1d100302fb43f3728d3be6649
nahmad94 <deevan11@gmail.com>
2022-12-23 15:08:22 -0500

    onchange and state set up on contact, working

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,23 +1,63 @@
-import React from 'react'
-import { Form } from 'semantic-ui-react'
+import React, {useState} from 'react'
+import { Form, Container, Input } from 'semantic-ui-react'
 
 export default function Contact() {
-  return (
-    <div>
-        <Form>
-            <Form.Group widths='equal'>
-                <Form.Input fluid label='First Name' placeholder='First Name' />
-                <Form.Input fluid label='Last Name' placeholder='Last Name' />
-            </Form.Group>
-            <Form.Group inline>
-                <Form.Input fluid label='Company Name' placeholder='Company Name' />
-            </Form.Group>
-            <Form.TextArea label='Description' placeholder='Tell us a little bit about your issue' />
-            <Form.Checkbox label='I agree to the Terms of this contact form' />
-            <Form.Button>Submit</Form.Button>
-        </Form>
-        <h1>Connect with us!</h1>
+    const [firstName, setfirstName] = useState('')
+    const [lastName, setlastName] = useState('')
+    const [company, setCompany] = useState('')
+    const [email, setEmail] = useState('')
+    const [description, setDescription] = useState('')
+    function handleFirstName(e) {
+        e.preventDefault();
+        setfirstName(e.target.value)
+    }
+    function handleLastName(e) {
+        e.preventDefault();
+        setlastName(e.target.value)
+    }
+    function handleCompany(e) {
+        e.preventDefault();
+        setCompany(e.target.value)
+    }
+    function handleEmail(e) {
+        e.preventDefault();
+        setEmail(e.target.value)
+    }
+    function handleDescription(e) {
+        e.preventDefault();
+        setDescription(e.target.value)
+    }
+    console.log(description)
+    // console.log(email)
+    return (
         
-    </div>
-  )
+        <div>
+            <Container text>
+                <Form>
+                    <Form.Group widths='equal'>
+                        <Form.Input fluid label='First Name' placeholder='First Name' onChange={handleFirstName}/>
+                        <Form.Input fluid label='Last Name' placeholder='Last Name' onChange={handleLastName}/>
+                    </Form.Group>
+                    <Form.Group widths='equal'>
+                        <Form.Input fluid label='Company Name' placeholder='Company Name' onChange={handleCompany}/>
+                    </Form.Group>
+                    <Form.Field
+                        id='form-input-control-error-email'
+                        control={Input}
+                        label='Email'
+                        placeholder='joe@schmoe.com'
+                        error={{
+                            content: 'Please enter a valid email address',
+                            pointing: 'below',
+                        }}
+                        onChange={handleEmail}
+                    />
+                    <Form.TextArea label='Description' placeholder='Tell us a little bit about your issue' onChange={handleDescription}/>
+                    <Form.Checkbox label='I agree to the Terms of this contact form' />
+                    <Form.Button>Submit</Form.Button>
+                </Form>
+                <h1>Connect with us!</h1>
+            </Container>
+        </div>
+    )
 }
