43412332b9b192f2ae7d39ff06acf79bc76cbe26
nahmad94 <deevan11@gmail.com>
2022-12-23 10:44:22 -0500

    adds semantic ui, sets up contact component

---
+++ src/Components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+        <h1>Connect with us!</h1>
+        
+    </div>
+  )
+}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,12 +3,13 @@ import React from 'react'
 export default function Header() {
   return (
     <div>
+      <nav>
         <h1>S.O.S Technologies</h1>
         <a>About Us</a>
         <a>Projects</a>
         <a>Contact</a>
         <a>Careers</a>
-        
+      </nav>
     </div>
   )
 }
