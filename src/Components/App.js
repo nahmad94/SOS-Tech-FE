de5902d6586da4f8c64bef6436624492f72c798d
nahmad94 <deevan11@gmail.com>
2022-12-23 12:06:20 -0500

    creates contact form

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
 import Header from './Header'
 import Home from './Home'
+import Contact from './Contact';
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <h1>Linked</h1>
       <Header />
       <Home />
+      <Contact />
     </div>
   );
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Form } from 'semantic-ui-react'
 
 export default function Contact() {
   return (
     <div>
+        <Form>
+            <Form.Group widths='equal'>
+                <Form.Input fluid label='First Name' placeholder='First Name' />
+                <Form.Input fluid label='Last Name' placeholder='Last Name' />
+            </Form.Group>
+            <Form.Group inline>
+                <Form.Input fluid label='Company Name' placeholder='Company Name' />
+            </Form.Group>
+            <Form.TextArea label='Description' placeholder='Tell us a little bit about your issue' />
+            <Form.Checkbox label='I agree to the Terms of this contact form' />
+            <Form.Button>Submit</Form.Button>
+        </Form>
         <h1>Connect with us!</h1>
         
     </div>
