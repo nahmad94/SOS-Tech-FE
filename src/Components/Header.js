3e29476885f4f2dbe023673783f1a77f696b751f
nahmad94 <deevan11@gmail.com>
2022-12-23 10:30:49 -0500

    header set up

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <h1>S.O.S Technologies</h1>
+        <a>About Us</a>
+        <a>Projects</a>
+        <a>Contact</a>
+        <a>Careers</a>
+        
+    </div>
+  )
+}
