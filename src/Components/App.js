566bcc45baa6395a92c58fcfc721642650a6cb33
nahmad94 <deevan11@gmail.com>
2022-12-23 10:39:26 -0500

    adds content to Home

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './Header'
+import Home from './Home'
 
 function App() {
   return (
     <div className="App">
       <h1>Linked</h1>
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+
+  return (
+    <div>
+        <h1>Welcome to S.O.S Technologies!</h1>
+        <p>At S.O.S Technologies, our goal is to make our customers </p>
+    </div>
+  )
+}
