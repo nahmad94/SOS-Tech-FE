f7e7141c19faf97526fb4509c58e5c92ef439943
nahmad94 <deevan11@gmail.com>
2023-01-04 00:55:37 -0500

    eod stash

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,9 +6,10 @@ export default function Home() {
     <div>
         <h1>Welcome to S.O.S Technologies!</h1>
         <img></img>
-        <h2>Web design, custom applications for your buisness, spreadsheets, tech help desk</h2>
-        {/* mission statement */}
-        <p>At S.O.S Technologies, our goal is to </p>
+        <div>We're a team of passionate technologists dedicated to helping businesses solve their toughest tech challenges. Whether you need a new website, a custom software solution, or expert IT support, we've got you covered.</div>
+        <div>Our mission is simple: to provide reliable and innovative tech solutions that help our customers succeed. We believe that by empowering businesses with the right tools and support, we can help them achieve their goals and thrive in today's competitive landscape.</div>
+        <div>Thank you for choosing SOS Tech. We look forward to working with you and being a trusted partner in your success.</div>
+        <div>Sincerely,<br />The SOS Tech Team</div>
     </div>
   )
 }
